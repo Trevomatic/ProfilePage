311f6e4e273634269813e67f706702059963c50c
Trevor Hall <TrevorWHall@gmail.com>
2015-09-21 11:56:26 -0700

    Home page word scrolling

--- client/js/script.js
+++ client/js/script.js
@@ -6,4 +6,56 @@ $(document).ready(function(){
     $(target).animatescroll({padding:40});
   });
 
+
+    var description = "Full-Stack Web Developer",
+        name = 'Trevor Hall',
+        button = "<a class='btn btn-primary btn-lg'>LEARN MORE ABOUT ME</a>";
+        nameDiv = document.getElementById('name'),
+        descDiv = document.getElementById('desc'),
+        buttonDiv = document.getElementById('button'),
+        character_counter = 0,
+        nameInterval = setInterval(updateName, 300),
+        descInterval = 0;
+        nameDiv.innerHTML = '&nbsp;';
+        descDiv.innerHTML = '&nbsp;';
+        buttonDiv.innerHTML = '&nbsp;';
+
+    function updateName()
+    {
+      if(character_counter < name.length) {
+        if(name[character_counter] == ' ') {
+          nameDiv.innerHTML = nameDiv.innerHTML+'&nbsp;';
+        }
+        else {
+          nameDiv.innerHTML = nameDiv.innerHTML+name[character_counter];
+        }
+      }
+      character_counter++;
+      if(character_counter == name.length + 3){  
+        clearInterval(nameInterval);
+        descInterval = setInterval(updateDesc, 300);
+        character_counter = 0;
+      }
+    }
+
+    function updateDesc()
+    {
+      if(character_counter < description.length) {
+        if(description[character_counter] == ' ') {
+          descDiv.innerHTML = descDiv.innerHTML+'&nbsp;';
+        }
+        else {
+          descDiv.innerHTML = descDiv.innerHTML+description[character_counter];
+        }
+      }
+      character_counter++;
+      if(character_counter == description.length + 5){  
+        clearInterval(descInterval);
+        buttonDiv.innerHTML = button;
+      }
+    }
+
+    
+
+
 });
--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, './client')));
 
-app.listen(8888, function(){
-	console.log('You are tuned in to port 8888');
+app.listen(8000, function(){
+	console.log('You are tuned in to port 8000');
 })
